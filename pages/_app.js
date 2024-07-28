0d342f52dbaac6b13d06a31561f33c46d0cf8b6d
abbasidd <usman@block360.io>
2024-07-28 20:48:31 +0500

    Header addition

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import "@/styles/globals.css";
+import '../styles/globals.css'
+// import { MoralisProvider } from 'react-moralis'
+// import { GunProvider } from '../context/gunContext'
+// import { CoinMarketProvider } from '../context/context'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    // <MoralisProvider
+    //   serverUrl='https://gqasdf4tkkiu.usemoralis.com:2053/server'
+    //   appId='TL0YJzCoVXgYv0fMKfzmgD52UaoaMllZI1DxTJWJ'
+    // >
+    //   <GunProvider>
+    //     <CoinMarketProvider>
+    // <>
+          <Component {...pageProps} />
+    // {/* </> */}
+    //     {/* </CoinMarketProvider>
+    //   </GunProvider>
+    // </MoralisProvider> */}
+  )
 }
+
+export default MyApp
